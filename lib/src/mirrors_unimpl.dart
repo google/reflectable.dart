968468faf53e7b29408085264e3a6ba783d38f05
Sigurd <sigurdm@google.com>
2015-03-24 13:03:04 +0100

    Fix imports in mirrors_unimpl
    BUG=
    R=eernst@google.com
    
    Review URL: https://codereview.chromium.org/1029093007

--- lib/src/mirrors_unimpl.dart
+++ lib/src/mirrors_unimpl.dart
@@ -2,8 +2,7 @@
 // source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-import 'dart:async';
-import '../reflectable.dart';
+import '../mirrors.dart';
 
 // Mirror classes with default implementations of all methods, to be used as
 // superclasses of transformer generated static mirror classes.  They serve to
