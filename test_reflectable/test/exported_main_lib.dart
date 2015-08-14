0e5da5f336dcac5b4c1186bb3940f40b9e471eb5
Sigurd <sigurdm@google.com>
2015-08-14 12:52:29 +0200

    Support the case where the entry-point file exports its main rather than declaring it.
    This changes the output file-layout a bit. The readme description is
    updated to reflect this.
    
    This removes the last use-case of SourceManager, so it is removed.
    
    BUG=
    R=eernst@google.com
    
    Review URL: https://codereview.chromium.org/1281533002 .

---
+++ test_reflectable/test/exported_main_lib.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2015, the Dart Team. All rights reserved. Use of this
+// source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+library test_reflectable.exported_main_lib;
+
+import "package:unittest/unittest.dart";
+
+import "package:reflectable/reflectable.dart";
+
+class Reflector extends Reflectable {
+  const Reflector() : super();
+}
+
+@Reflector()
+class C {}
+
+main() {
+  test("exported main", () {
+    expect(const Reflector().canReflectType(C), true);
+  });
+}
