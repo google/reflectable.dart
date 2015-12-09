f9502437b3c33107a49413a1fca143d001a1d217
Erik Ernst <eernst@google.com>
2015-12-09 09:23:33 +0100

    Confirms that the inherited-from-non-covered class bug is fixed.
    This CL adds a test 'inherited_variable_test.dart' which fails in
    v0.4.0 and succeeds in the most recent commit
    5a835be813daeac500ec3f7ccc8fef6e20c58e30, thus demonstrating that
    this commit contains changes addressing this issue (as mentioned in
    a comment on issue #51).
    
    Fixes issue #51.
    
    R=sigurdm@google.com
    
    Review URL: https://codereview.chromium.org/1509673003 .

---
+++ test_reflectable/test/inherited_variable_test.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2015, the Dart Team. All rights reserved. Use of this
+// source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+// File being transformed by the reflectable transformer.
+// Uses a getter/setter for a variable which is inherited
+// from a non-covered class. Created for issue #51, based on
+// input from Lasse Damgaard.
+
+library test_reflectable.test.inherited_variable_test;
+
+import 'package:reflectable/reflectable.dart';
+import 'package:unittest/unittest.dart';
+
+class Reflector extends Reflectable {
+  const Reflector()
+      : super(instanceInvokeCapability, metadataCapability, nameCapability);
+}
+
+const reflector = const Reflector();
+
+class A {
+  int x;
+}
+
+class B extends A {}
+
+@reflector
+class C extends B {}
+
+main() {
+  test('Variable inherited from non-covered class', () {
+    expect(
+        reflector.reflect(new C()).type.instanceMembers['x'].simpleName, 'x');
+  });
+}
