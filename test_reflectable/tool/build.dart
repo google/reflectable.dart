d3d8332aeb53b2ce71ae2d08df70ce12fbe23186
Erik Ernst <eernst@google.com>
2017-12-08 10:22:21 +0100

    Now using glob in build.dart, such that it can be invoked without an intermediate shell globbing step

--- test_reflectable/tool/build.dart
+++ test_reflectable/tool/build.dart
@@ -2,8 +2,16 @@
 // source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
+import 'package:glob/glob.dart';
 import 'package:reflectable/reflectable_builder.dart' as builder;
 
 main(List<String> arguments) async {
-  await builder.reflectableBuild(arguments);
+  List<String> globbedArguments = <String>[];
+  for (String argument in arguments) {
+    String fileSpec = new Glob(argument);
+    await for (FileSystemEntity entity in fileSpec.list()) {
+      globbedArguments.add(entity.path);
+    }
+  }
+  await builder.reflectableBuild(globbedArguments);
 }
