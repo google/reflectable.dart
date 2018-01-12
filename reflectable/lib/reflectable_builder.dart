e3dce1bc879948693fc8a3b42610f9929ea42ed9
Jacob MacDonald <jakemac@google.com>
2018-01-12 07:28:06 -0800

    update to latest package:build and add custom build.yaml to auto-apply the builder

--- reflectable/lib/reflectable_builder.dart
+++ reflectable/lib/reflectable_builder.dart
@@ -6,9 +6,22 @@ library reflectable.src.reflectable_builder;
 
 import 'dart:async';
 import 'package:barback/src/transformer/barback_settings.dart';
+import 'package:build/build.dart';
+import 'package:build_config/build_config.dart';
 import 'package:build_barback/build_barback.dart';
 import 'package:build_runner/build_runner.dart';
 import 'package:reflectable/transformer.dart';
+
+TransformerBuilder reflectableBuilder(BuilderOptions options) {
+  var config = new Map<String, Object>.from(options.config);
+  config.putIfAbsent('entry_points', () => ['**.dart']);
+  return new TransformerBuilder(
+      new ReflectableTransformer.asPlugin(
+          new BarbackSettings(config, BarbackMode.DEBUG)),
+      const <String, List<String>>{
+        '.dart': const ['.reflectable.dart']
+      });
+}
 
 Future<BuildResult> reflectableBuild(List<String> arguments) async {
   if (arguments.length < 1) {
@@ -17,8 +30,6 @@ Future<BuildResult> reflectableBuild(List<String> arguments) async {
     print("reflectable_builder: No arguments given, exiting.");
     return new BuildResult(BuildStatus.success, []);
   } else {
-    PackageGraph graph = new PackageGraph.forThisPackage();
-    String packageName = graph.root.name;
     // TODO(eernst) feature: We should support some customization of
     // the settings, e.g., specifying options like `suppress_warnings`.
     BarbackSettings settings = new BarbackSettings(
@@ -27,10 +38,12 @@ Future<BuildResult> reflectableBuild(List<String> arguments) async {
     );
     final builder = new TransformerBuilder(
       new ReflectableTransformer.asPlugin(settings),
-      const <String, List<String>>{'.dart': const ['.reflectable.dart']},
+      const <String, List<String>>{
+        '.dart': const ['.reflectable.dart']
+      },
     );
     return await build(
-        [new BuildAction(builder, packageName, inputs: arguments)],
+        [applyToRoot(builder, generateFor: new InputSet(include: arguments))],
         deleteFilesByDefault: true);
   }
 }
