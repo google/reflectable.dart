e3e4ffb8754bebd56d57a941224591190dff321b
Erik Ernst <eernst@google.com>
2020-12-01 12:53:03 +0100

    Mark test_reflectable tests as legacy

--- test_reflectable/test/meta_reflector_test.dart
+++ test_reflectable/test/meta_reflector_test.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2015, the Dart Team. All rights reserved. Use of this
 // source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
+// @dart=2.9
 
 /// File used to test reflectable code generation.
 /// Creates a `MetaReflector` which may be used to reflect on the set of
