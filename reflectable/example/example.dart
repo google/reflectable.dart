e1c7dc03b52c3b683dd358b0aa501577e8c74459
Erik Ernst <eernst@google.com>
2020-12-22 00:28:02 +0100

    Updated example.dart

--- reflectable/example/example.dart
+++ reflectable/example/example.dart
@@ -20,8 +20,8 @@ class A {
   int arg1(int x) => x - 42;
   int arg1to3(int x, int y, [int z = 0, w]) => x + y + z * 42;
   int argNamed(int x, int y, {int z = 42}) => x + y - z;
-  int operator +(x) => 42 + x;
-  int operator [](x) => 42 + x;
+  int operator +(int x) => 42 + x;
+  int operator [](int x) => 42 + x;
   void operator []=(x, v) {
     f = x + v;
   }
@@ -37,41 +37,41 @@ class A {
   static int namedArguments(int x, int y, {int z = 42}) => x + y - z;
 }
 
-main() {
+void main() {
   // The program execution must start run this initialization before
   // any reflective features can be used.
   initializeReflectable();
 
   // Get hold of a few mirrors.
-  A instance = A();
-  InstanceMirror instanceMirror = myReflectable.reflect(instance);
-  ClassMirror classMirror = myReflectable.reflectType(A);
+  var instance = A();
+  var instanceMirror = myReflectable.reflect(instance);
+  var classMirror = myReflectable.reflectType(A) as ClassMirror;
 
   // Invocations of methods accepting positional arguments (printing '42').
-  print(instanceMirror.invoke("arg0", []));
-  print(instanceMirror.invoke("arg1", [84]));
-  print(instanceMirror.invoke("arg1to3", [40, 2]));
-  print(instanceMirror.invoke("arg1to3", [1, -1, 1]));
-  print(instanceMirror.invoke("arg1to3", [21, 21, 0, "foo"]));
+  print(instanceMirror.invoke('arg0', []));
+  print(instanceMirror.invoke('arg1', [84]));
+  print(instanceMirror.invoke('arg1to3', [40, 2]));
+  print(instanceMirror.invoke('arg1to3', [1, -1, 1]));
+  print(instanceMirror.invoke('arg1to3', [21, 21, 0, 'foo']));
 
   // Invocations of methods accepting named arguments (printing '42').
-  print(instanceMirror.invoke("argNamed", [55, 29]));
-  print(instanceMirror.invoke("argNamed", [21, 21], {#z: 0}));
+  print(instanceMirror.invoke('argNamed', [55, 29]));
+  print(instanceMirror.invoke('argNamed', [21, 21], {#z: 0}));
 
   // Invocations of operators.
-  print(instanceMirror.invoke("+", [42])); // '84'.
-  print(instanceMirror.invoke("[]", [42])); // '84'.
-  instanceMirror.invoke("[]=", [1, 2]);
+  print(instanceMirror.invoke('+', [42])); // '84'.
+  print(instanceMirror.invoke('[]', [42])); // '84'.
+  instanceMirror.invoke('[]=', [1, 2]);
   print(instance.f); // '3'.
-  print(instanceMirror.invoke("unary-", [])); // '-3'.
-  print(instanceMirror.invoke("~", [])); // '5'.
+  print(instanceMirror.invoke('unary-', [])); // '-3'.
+  print(instanceMirror.invoke('~', [])); // '5'.
 
   // Similar invocations on static methods (printing '42').
-  print(classMirror.invoke("noArguments", []));
-  print(classMirror.invoke("oneArgument", [84]));
-  print(classMirror.invoke("optionalArguments", [40, 2]));
-  print(classMirror.invoke("optionalArguments", [1, -1, 1]));
-  print(classMirror.invoke("optionalArguments", [21, 21, 0, "foo"]));
-  print(classMirror.invoke("namedArguments", [55, 29]));
-  print(classMirror.invoke("namedArguments", [21, 21], {#z: 0}));
+  print(classMirror.invoke('noArguments', []));
+  print(classMirror.invoke('oneArgument', [84]));
+  print(classMirror.invoke('optionalArguments', [40, 2]));
+  print(classMirror.invoke('optionalArguments', [1, -1, 1]));
+  print(classMirror.invoke('optionalArguments', [21, 21, 0, 'foo']));
+  print(classMirror.invoke('namedArguments', [55, 29]));
+  print(classMirror.invoke('namedArguments', [21, 21], {#z: 0}));
 }
