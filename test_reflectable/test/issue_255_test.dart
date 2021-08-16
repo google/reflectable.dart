2d090fd599537337e529deb65cc0deb90b404cd7
Erik Ernst <eernst@google.com>
2021-08-16 18:33:50 +0200

    New test, exercising the situation reported in issue 255

---
+++ test_reflectable/test/issue_255_test.dart
@@ -0,0 +1,21 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Tests a situation where a top level variable needs to have `ownerIndex`
+// `NO_CAPABILITY_INDEX` (because there is no support for accessing the
+// corresponding library mirror). This used to cause a null error.
+
+import 'package:reflectable/reflectable.dart';
+import 'scratch_test.reflectable.dart';
+
+@c
+class C extends Reflectable {
+  const C() : super(topLevelInvokeCapability);
+}
+
+const c = C();
+
+void main() {
+  initializeReflectable();
+}
